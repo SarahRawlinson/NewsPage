255e748b35686b345cd5dff4bc47faaffa3c4a3c
Sarah Rawlinson <sarahrosannerawlinson@gmail.com>
2023-02-05 22:25:51 +0000

    added search

--- index.js
+++ index.js
@@ -1,88 +1,112 @@
-﻿const query = "bob";
-const from = "2023-02-04";
-const to = "2023-02-05";
-const sortBy = "popularity";
-$.ajax({
-    type: "POST",
-    url: "http://localhost:3000",
-    data: {
-        query: query,
-        from: from,
-        to: to,
-        sortBy: sortBy
-    },
-    success: function(data) {
-        console.log(data);
-        display_news(data.articles);
-    },
-    error: function(
-        error) {
-        console.error("An error occurred while making the AJAX request:", error);
-    }
-});
+﻿
 
 
 function display_news(articles) {
-    // console.log(articles);
     const newsContainer = document.getElementById("news-items");
+    newsContainer.classList.add("news-items", "d-flex", "flex-wrap", "justify-content-center");
+
+    function addElement(parent, tag, classes, content) {
+        const element = document.createElement(tag);
+        element.classList.add(...classes);
+        element.textContent = content;
+        parent.appendChild(element);
+        return element;
+    }
 
     for (let i = 0; i < articles.length; i++) {
         const article = articles[i];
-        // console.log(article);
-        const newsItem = document.createElement("div");
-        newsItem.classList.add("news-item", "card", "mb-3");
 
-        const newsBody = document.createElement("div");
-        newsBody.classList.add("card-body");
+        const newsItem = addElement(newsContainer, "div", ["news-item", "card", "m-1", "p-1", "col-12", "col-sm-12", "col-md-8", "col-lg-3"], "");
 
-        //title
-        const title = document.createElement("h5");
-        title.classList.add("card-title");
-        title.textContent = article['title'];
-        newsBody.appendChild(title);
+        const newsBody = addElement(newsItem, "div", ["card-body", "news-body"], "");
 
-        //author
-        if (article['author']) {
-            const author = document.createElement("p");
-            author.classList.add("card-text", "news-author");
-            author.textContent = article['author'];
-            newsBody.appendChild(author);
+        const title = addElement(newsBody, "h5", ["card-title", "news-title"], `Title: ${article.title}`);
+        title.setAttribute("id", "title");
+
+        if (article.author) {
+            const author = addElement(newsBody, "p", ["card-text", "news-author", "font-italic"], `Author: ${article.author}`);
+            author.setAttribute("id", "author");
         }
 
-        if (article['source']) {
-            
-            if (article['source']['Name']) {
-                console.log(article['source']['Name']);
-                const name = document.createElement("p");
-                name.classList.add("card-text");
-                name.textContent = article['source']['Name'];
-                newsBody.appendChild(name);
+        if (article.source) {
+            const source = addElement(newsBody, "div", ["card-body", "news-source"], "");
+            if (article.source.Name) {
+                addElement(source, "p", ["card-text", "news-name"], `Name: ${article.source.Name}`);
             }
-            if (article['source']['Id']) {
-                console.log(article['source']['Id']);
-                const id = document.createElement("p");
-                id.classList.add("card-text");
-                id.textContent = article['source']['Id'];
-                newsBody.appendChild(id);
+            if (article.source.Id) {
+                addElement(source, "p", ["card-text", "news-id"], `ID: ${article.source.Id}`);
             }
         }
 
-        if (article['url']) {
-            const url = document.createElement("a");
-            url.classList.add("card-text");
-            url.textContent = article['url'];
-            url.href = article['url'];
-            newsBody.appendChild(url);
+        if (article.url) {
+            const url = addElement(newsBody, "a", ["card-text", "news-url"], convertUrlToName(article.url));
+            url.setAttribute("href", article.url);
         }
 
-        if (article['publishedAt']) {
-            const publishedAt = document.createElement("p");
-            publishedAt.classList.add("card-text");
-            publishedAt.textContent = article['publishedAt'];
-            newsBody.appendChild(publishedAt);
+        if (article.publishedAt) {
+            const publishedAt = addElement(newsItem, "p", ["card-text", "news-published-at", "d-flex", "flex-wrap", "justify-content-end"], `Published At: ${article.publishedAt}`);
+            publishedAt.setAttribute("id", "publishedAt");
         }
-
-        newsItem.appendChild(newsBody);
-        newsContainer.appendChild(newsItem);
     }
 }
+
+function convertUrlToName(url) {
+    // Removes the protocol (e.g. "http://" or "https://")
+    url = url.replace(/(^\w+:|^)\/\//, '');
+    // Removes the www.
+    url = url.replace(/^www\./, '');
+    // Removes everything after the first slash
+    url = url.split('/')[0];
+    // Replaces dashes with spaces
+    url = url.replace(/-/g, ' ');
+    // Capitalizes first letter of each word
+    url = url.replace(/\b\w/g, function(l) { return l.toUpperCase() });    
+    return url;
+}
+$( function() {
+    $( "#from" ).datepicker();
+    $( "#to" ).datepicker();
+} );
+
+
+window.onload = function() {
+    
+    document.getElementById("searchButton").addEventListener("click", function () {
+        console.log('start search');
+        const enteredQuery = document.getElementById("query").value;
+        const enteredFrom = document.getElementById("from").value;
+        const enteredTo = document.getElementById("to").value;
+
+        document.getElementById("enteredQuery").innerHTML = enteredQuery;
+        document.getElementById("enteredFrom").innerHTML = enteredFrom;
+        document.getElementById("enteredTo").innerHTML = enteredTo;
+
+        searchNews(enteredQuery, enteredFrom, enteredTo);
+        console.log('end search');
+    })
+}
+
+function searchNews(query, from, to) {
+    // Your code to search for news goes here
+    console.log(`Searching for news with query: ${query}, from: ${from}, to: ${to}`);
+    const sortBy = "popularity";
+    $.ajax({
+        type: "POST",
+        url: "http://localhost:3000",
+        data: {
+            query: query,
+            from: from,
+            to: to,
+            sortBy: sortBy
+        },
+        success: function(data) {
+            console.log(data);
+            display_news(data.articles);
+        },
+        error: function(
+            error) {
+            console.error("An error occurred while making the AJAX request:", error);
+        }
+    });
+}
+
--- index.php
+++ index.php
@@ -1,17 +1,86 @@
 ﻿<!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <link
+            rel="stylesheet"
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" 
+            integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
+            crossorigin="anonymous"
+    />
     <title>News API Results</title>
     <link rel="stylesheet" type="text/css" href="style.css">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="index.js"></script>
+    <script
+            src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+            crossorigin="anonymous"
+    ></script>
+    <script
+            src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"
+            integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p"
+            crossorigin="anonymous"
+    ></script>
+    <script
+            src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"
+            integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF"
+            crossorigin="anonymous"
+    ></script>
+    
+<!--    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>-->
+    <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
 </head>
 <body>
 <div class="news-container">
-    <h2>News Results</h2>
+    <h2 class="h2 d-flex flex-wrap justify-content-center  p-5 m-5" id="title">News Results</h2>
+    <div class="container d-flex flex-wrap justify-content-center">
+        <form class="col-6">
+            <div class="form-group col-12">
+                <label for="query">Search For:</label>
+                <input type="text" class="form-control" id="query">
+            </div>
+
+            <div class="form-group col-12">
+                <label for="from">From:</label>
+                <input type="text" class="form-control" id="from">
+            </div>
+
+            <div class="form-group col-12">
+                <label for="to">To:</label>
+                <input type="text" class="form-control" id="to">
+            </div>
+
+            <div class="d-flex flex-wrap justify-content-end">
+                <button type="button" class="btn btn-primary m-2 ms-5 me-5 p-2" id="searchButton" style="width: 20%;">
+                    Search
+                </button>
+            </div>
+            
+        </form>
+        <form class="col-4 form-search-values position-absolute-right m-5 p-5">
+
+            <div class="form-group row">
+                <label>Entered Query:</label>
+                <span id="enteredQuery"></span>
+            </div>
+
+            <div class="form-group row">
+                <label>Entered From:</label>
+                <span id="enteredFrom"></span>
+            </div>
+
+            <div class="form-group row">
+                <label>Entered To:</label>
+                <span id="enteredTo"></span>
+            </div>
+        </form>
+    </div>
     <div id="news-items"></div>
 </div>
 
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<script src="index.js"></script>
+
 </body>
 </html>
